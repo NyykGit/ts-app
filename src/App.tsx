1e29a1455b84b069178ebb663750e6f4b02d2b0d
Nick Reifler <Nick.Reifler@netcetera.com>
2020-06-10 13:49:07 +0200

    [no-issue] setup

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,20 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          TypeScript - Hello World
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
+/*
+<firmenname-counter></firmenname-counter>
+asdf
+gitignore
+devdocs
+
+GET /users/{id}
+*/
